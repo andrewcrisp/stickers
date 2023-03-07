2cb6176505ca4c5fd4c81037185165e4c66f5709
Andrew Crisp <andrewcrisp556@hotmail.com>
2023-03-07 13:07:30 -0700

    Add storefront and laminator costs

--- server.R
+++ server.R
@@ -7,20 +7,24 @@ server <- function(input, output) {
   output$pricePlot <- renderPlot({
     debt_function <- function(unitCount) {
       input$PrinterCost + input$CutterCost + input$LaminatorCost +
-        input$NumberOfInkCartridges * (input$PriceOfInkCartridge * (ceiling(unitCount / input$UnitsPerInkCartridge))) +
-        input$PriceOfPaper * (ceiling(input$LengthPerOrderInches * 12) / input$LengthOfPaperRollFeet) +
-        input$LaminatorRollCost * (ceiling((input$LengthPerOrderInches + input$LaminatorSheetMarginInch) * 12) / input$LengthOfLamnatorRollFeet)
+        (input$NumberOfInkCartridges * (input$PriceOfInkCartridge * (ceiling(unitCount / input$UnitsPerInkCartridge)))) +
+        (input$PriceOfPaper * (ceiling(input$LengthPerOrderInches * 12) / input$LengthOfPaperRollFeet)) +
+        (input$LaminatorRollCost * (ceiling((input$LengthPerOrderInches + input$LaminatorSheetMarginInch) * 12) / input$LengthOfLamnatorRollFeet)) +
+        (input$StorefrontTransactionPercentage * .01 * unitCount)
     }
     
     unitsSold <- unitsSold %>% 
       mutate(totalCost = debt_function(units)) %>% 
-      mutate(grossSales = PricePoint  * units) %>% 
+      mutate(grossSales = input$PricePoint  * units) %>% 
       mutate(net = grossSales - totalCost) %>% 
       mutate(netPerUnit = net / units)
     
     unitsSold %>% ggplot(aes(x = units, y = net)) +
       geom_line() +
-      geom_hline(aes(yintercept=0), color = "red")
+      geom_hline(aes(yintercept=0), color = "red") 
+      #geom_line(aes(x = units, y = totalCost), color = "gray") +
+      #geom_line(aes(x = units, y = grossSales), color = "green")
+    
   })
   
 }
